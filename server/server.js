2bc075f0db9af1e4e89a9a2f42940bc0a9cbf8d1
IvoGoman <IvoGoman@users.noreply.github.com>
2016-10-25 20:41:39 +0200

    Uncommented Body Parser

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 //Dependencies
 var express = require('express');
-//var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 
 //Express Setup
 var app = express();
 
-/*
+
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());*/
+app.use(bodyParser.json());
+
 
 //API Routing
 app.use('/api/v1/video', require('./routes/video'));
